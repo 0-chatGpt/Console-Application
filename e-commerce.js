6a2b4b4460824c2cdcd54aba07136d4dc166e3ca
Chinemeremun Okereke <chinemeremun.okereke.236535@unn.ng>
2024-01-20 23:38:32 +0100

    Established the prompt function

--- e-commerce.js
+++ e-commerce.js
@@ -1,2 +1,21 @@
 "use strict";
 
+const readline = require("readline");
+
+
+function prompt(query){
+  // Pseudo synchronous Behaviour based on Asynchronous behaviour
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  return new Promise((resolve, reject) => {
+    rl.question(query, (answer)=>{
+      rl.close();
+      resolve(answer);
+    });
+    reject('I/O error encountered Please restart Application');
+  });
+}
+
